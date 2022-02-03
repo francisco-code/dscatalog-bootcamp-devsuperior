d595ea9ab446dc4c54aca39ca8700e48aef87204
Felipe Correia <felipedreamcorreia@hotmail.com>
2022-02-03 00:39:31 -0300

    feat: insert ControllerAdvice

---
+++ backend/src/main/java/com/felipecorreia/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.felipecorreia.dscatalog.resources.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+}
