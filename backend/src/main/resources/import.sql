418e1b848be37fa67844a472fedf855b8d01cc81
Felipe Correia <felipedreamcorreia@hotmail.com>
2022-02-10 00:42:29 -0300

    feat: audit data with UTC time

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_category(name) VALUES ('Books');
-INSERT INTO tb_category(name) VALUES ('Eletronics');
-INSERT INTO tb_category(name) VALUES ('Computers');
-INSERT INTO tb_category(name) VALUES ('Music');
-INSERT INTO tb_category(name) VALUES ('Games');
+INSERT INTO tb_category(name, created_At) VALUES ('Books', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Computers', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Music', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Games', NOW());
